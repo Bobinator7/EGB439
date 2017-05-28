1e23236f285bd168f2f6214fa4d6211a343e3f36
Bobinator7 <jsloh93@gmail.com>
2017-05-28 10:36:29 +1000

    change speed to power conversion

--- matlab/slam/robot/motion/v2pL.m
+++ matlab/slam/robot/motion/v2pL.m
@@ -1,3 +1,9 @@
 function pL = v2pL(v)
-    pL = int32(round(v * (-122.00) - 16.75));
+    if v > 0
+        pL = int32(round(v * (-122.00) - 16.75));
+    elseif v < 0
+        pL = int32(round(v * (-122.00) + 16.75));
+    else
+        pL = 0;
+    end
 end
--- matlab/slam/robot/motion/v2pR.m
+++ matlab/slam/robot/motion/v2pR.m
@@ -1,3 +1,9 @@
 function pR = v2pR(v)
-    pR = int32(round(v * (-116.6512) - 16.65));
+    if v > 0
+        pR = int32(round(v * (-116.6512) - 16.65));
+    elseif v < 0
+        pR = int32(round(v * (-116.6512) + 16.65));
+    else
+        pR = 0;
+    end
 end
